2194f9796ea69042f486d56e84582e5284e3d20c
Hannah Malm <hannahemalm616@gmail.com>
2021-11-29 12:07:30 -0500

    updated environment file using require all gem

--- config/envrionment.rb
+++ config/envrionment.rb
@@ -2,5 +2,12 @@ require 'bundler'
 Bundler.require 
 
 configure :development do 
-    set :database, {adapter: "sqlite3", database: "db/database.sqlite3"}
+    set :database, {adapter: "sqlite3", database: "db/run.sqlite3"}
 end 
+
+
+
+# require_relative '../app/controllers/application_controller.rb'
+# require_relative '../app/controllers/logs_controller.rb'
+# require_relative '../app/controllers/users_controller.rb'
+require_all 'app'
