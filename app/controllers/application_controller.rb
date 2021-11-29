f2caccb755e5d7ad636b97bf2b32a3def69d75a7
Hannah Malm <hannahemalm616@gmail.com>
2021-11-29 12:44:18 -0500

    update configure do to enable sessions and register the flash for flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 require "./config/envrionment"
 
 class ApplicationController < Sinatra::Base 
+    configure do  #configure block tells controller where to look for views and enable sessions
+        set :public_folder, 'public'
+        set :views, "app/views"
+        enable :sessions
+        set :session_secret, "password_security" #extra layer of security
+        register Sinatra::Flash #sinatra takes this info and turns it into a hash
+    end 
+
+   
 
 
 end 
