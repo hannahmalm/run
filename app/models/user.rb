aa810afed5e40d87660ad6f1b54d257b806f106a
Hannah Malm <hannahemalm616@gmail.com>
2021-11-29 19:34:02 -0500

    create log model and updated relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
     has_secure_password #allows us to use authenticate which takes pass as plain string and allows you to check against Bcrypts hash
+    has_many_logs
 end 
