beeacf96cd860340dd1cbe95e4c703905a15296b
Hannah Malm <hannahemalm616@gmail.com>
2021-11-29 20:58:36 -0500

    enabled sinatra flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,13 @@ class UsersController < ApplicationController
          @user = User.find_by(username: params[:username])
          #authenticate the user - this works with has_secure_password - verify the user is who they say they are
          if @user && @user.authenticate(password: params[:password])
-         #log the user in
-         session[:user_id] = user.id
-         #redirect to users landing page
-         redirect to "/users/#{user.id}"
+            #log the user in
+            session[:user_id] = user.id
+            #redirect to users landing page
+            redirect to "/users/#{user.id}"
+         else 
+            flash[:message] = "Please enter a valid Username or Password" #flash error - render in view
+            redirect to '/login' #redirect to login
     end 
 
     get '/signup' do #render the signup form 
