fa7b4fcd2cc57355353b7a8b3037957b307b870f
Hannah Malm <hannahemalm616@gmail.com>
2021-11-29 20:35:49 -0500

    added login and signup routes and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,11 @@
 class UsersController < ApplicationController
 
+    get '/login' do #render the login form
+        erb :login
+    end 
+
+    get '/signup' do #render the signup form 
+        erb :signup
+    end 
+
 end 
