d59cba5667a6e7f052dc94818915f58ad4642338
Hannah Malm <hannahemalm616@gmail.com>
2021-11-29 20:44:26 -0500

    added in post login route and form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
         erb :login
     end 
 
+    post '/login' do #post - creating a key value pair to session hash, receive log in form, find the user, log the user in
+
+    end 
+
     get '/signup' do #render the signup form 
         erb :signup
     end 
