591fd63b910116ec5be9d7735098d19b4f7ca544
Hannah Malm <hannahemalm616@gmail.com>
2021-11-29 21:13:13 -0500

    updated post login with flash and redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,13 @@ class UsersController < ApplicationController
          #authenticate the user - this works with has_secure_password - verify the user is who they say they are
          if @user && @user.authenticate(password: params[:password])
             #log the user in
-            session[:user_id] = user.id
-            #redirect to users landing page
-            redirect to "/users/#{user.id}"
+            session[:user_id] = @user.id
+            #redirect to users landing page via dynamic route
+            redirect to "/users/#{@user.id}"
          else 
             flash[:message] = "Please enter a valid Username or Password" #flash error - render in view
             redirect to '/login' #redirect to login
+         end 
     end 
 
     get '/signup' do #render the signup form 
