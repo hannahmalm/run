fcc7ed891c3df7ebe8a41fa154ecd1774382c887
Hannah Malm <hannahemalm616@gmail.com>
2021-11-29 21:16:48 -0500

    updated all routes on users controller to double quotes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
 
-    get '/login' do #render the login form
+    get "/login" do #render the login form
         erb :login
     end 
 
-    post '/login' do #post - creating a key value pair to session hash, receive log in form, find the user, log the user in
+    post "/login" do #post - creating a key value pair to session hash, receive log in form, find the user, log the user in
          #find the user
          @user = User.find_by(username: params[:username])
          #authenticate the user - this works with has_secure_password - verify the user is who they say they are
@@ -12,14 +12,18 @@ class UsersController < ApplicationController
             #log the user in
             session[:user_id] = @user.id
             #redirect to users landing page via dynamic route
-            redirect to "/users/#{@user.id}"
+            redirect to "users/#{@user.id}"
          else 
             flash[:message] = "Please enter a valid Username or Password" #flash error - render in view
             redirect to '/login' #redirect to login
          end 
     end 
 
-    get '/signup' do #render the signup form 
+    get "/users/:id" do #this is a User Show route 
+        "User Show Route"
+    end 
+
+    get "/signup" do #render the signup form 
         erb :signup
     end 
 
