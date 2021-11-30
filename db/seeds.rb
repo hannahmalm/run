6e807389012e3644f5da03dfb4347a6f863e949d
Hannah Malm <hannahemalm616@gmail.com>
2021-11-29 20:15:45 -0500

    commented out seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 #create seed data and test associations
 
 #create 2 users
-#User.create(username: "Hannah", password: "password")
-#User.create(username: "Hank", password: "password")
+# User.create(username: "Hannah", password: "password")
+# User.create(username: "Hank", password: "password")
 
-#create 2 logs
-#Log.create(date: "12-1-2021", distance: "10.2", pace: "9'45", avg_heart_rate: "105", notes: "easy run", user_id: hannah.id)
-#Log.create(date: "12-10-2021", distance: "2.3", pace: "10'45", avg_heart_rate: "195", notes: "easy run", user_id: hank.id)
+# #create 2 logs
+# Log.create(date: "12-1-2021", distance: "10.2", pace: "9'45", avg_heart_rate: "105", notes: "easy run", user_id: Hannah.id)
+# Log.create(date: "12-10-2021", distance: "2.3", pace: "10'45", avg_heart_rate: "195", notes: "easy run", user_id: Hank.id)
 
