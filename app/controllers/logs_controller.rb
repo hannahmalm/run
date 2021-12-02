b8dd81ee44217e4220badfd2354195b23bfc408f
Hannah Malm <hannahemalm616@gmail.com>
2021-12-02 18:27:44 -0500

    added in a new route for post logs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -7,11 +7,15 @@ class LogsController < ApplicationController
     end 
 
     post '/logs' do 
-        not_logged_in_helper #check to see if user is logged in
-        @log = Log.new(params)
-        if @log.save 
-            redirect "/logs/#{@log.id}"
-            #redirect "/logs/index"
+        not_logged_in_helper #check to see if user is logged in  - if not logged in redirect
+        # @log = Log.new(params)
+        # if @log.save 
+        #     redirect "/logs/#{@log.id}"
+        #     #redirect "/logs/index"
+        # else 
+        if params[:date] != "" && params[:distance] != "" && params[:pace] != ""
+            @log = Log.create(date: params[:date], distance: params[:distance], pace: params[:pace], avg_heart_rate: params[:avg_heart_rate], notes: params[:notes], user_id: current_user.id)
+            redirect to "/logs/#{@log.id}"
         else 
             #provide flash errors if logs are not completed - date, distance, pace are required
             flash[:errors] = "#{@log.errors.full_messages.to_sentence}"
