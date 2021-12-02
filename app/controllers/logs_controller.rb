29c2d2865ed202af3c600431e2ffa9eb392d3817
Hannah Malm <hannahemalm616@gmail.com>
2021-12-01 19:56:30 -0500

    added in log.save, redirect and flash errors for creating a new log

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,7 +2,26 @@ class LogsController < ApplicationController
 
     #render a form for logs
     get '/logs/new' do 
+        not_logged_in_helper #check to see if user is logged in
         erb :'/logs/new'
+    end 
+
+    post '/logs' do 
+        not_logged_in_helper #check to see if user is logged in
+        @log = Log.new(params)
+        if @log.save 
+            redirect "/logs/#{@log.id}"
+            #redirect "/logs/index"
+        else 
+            #provide flash errors if logs are not completed - date, distance, pace are required
+            flash[:errors] = "Signup Failure: #{@log.errors.full_messages.to_sentence}"
+            redirect to '/logs/new'
+        end 
+
+    end 
+
+    get '/log/:id'
+        "Test"
     end 
 
 end 
