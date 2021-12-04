a3243cad3276f4d1d36f53d4ab968d296b0627d3
Hannah Malm <hannahemalm616@gmail.com>
2021-12-04 13:49:38 -0500

    commented out validates Log and trying it manually

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,6 +1,6 @@
 class Log < ActiveRecord::Base
     belongs_to :user
-    validates :date, presence: true
-    validates :distance, presence: true
-    validates :pace, presence: true
+    # validates :date, presence: true
+    # validates :distance, presence: true
+    # validates :pace, presence: true
 end 
