39b2e369d5d37b7f0bcbbd14d608e29db5891629
Hannah Malm <hannahemalm616@gmail.com>
2021-12-05 17:04:21 -0500

    date cannot be in the future

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,4 +3,13 @@ class Log < ActiveRecord::Base
     # validates :date, presence: true
     # validates :distance, presence: true
     # validates :pace, presence: true
+
+    validate :date_cannot_be_in_the_future
+ 
+
+  def date_cannot_be_in_the_future
+    if date.present? && date > Date.today
+      errors.add(:date, "Date cannot be in the future")
+    end
+  end
 end 
