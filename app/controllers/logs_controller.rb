a9f31dbcfccd7a64f8d93a8261176832d6334998
Hannah Malm <hannahemalm616@gmail.com>
2021-12-05 17:32:39 -0500

    deleted unused code

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -16,9 +16,6 @@ class LogsController < ApplicationController
 
     post '/logs' do 
         not_logged_in_helper #check to see if user is logged in  - if not logged in redirect
-        # @log = Log.new(params)
-        # if @log.save 
-        #     redirect "/logs/#{@log.id}"
         if params[:date] != "" && params[:distance] != "" && params[:pace] != "" 
             @log = Log.create(:date => params[:date], :distance => params[:distance], :pace => params[:pace], :avg_heart_rate => params[:avg_heart_rate], :notes => params[:notes], user_id: current_user.id)
             redirect to "/logs/#{@log.id}"
