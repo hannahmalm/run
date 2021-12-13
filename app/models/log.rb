9f736fa2ddafc13045c7932e19051d4bdcccd514
Hannah Malm <hannahemalm616@gmail.com>
2021-12-13 17:30:42 -0500

    deleted out unused validator

--- app/models/log.rb
+++ app/models/log.rb
@@ -5,13 +5,9 @@ class Log < ActiveRecord::Base
     validates :distance, presence: true
     validates :pace, presence: true
     validates :avg_heart_rate, numericality: { greater_than: 30}
-    validate :date_cannot_be_in_the_future
+   
  
-  def date_cannot_be_in_the_future
-    if date.present? && date > Date.today
-      errors.add(:date, "Date cannot be in the future")
-    end
-  end
+  
 
   
 
